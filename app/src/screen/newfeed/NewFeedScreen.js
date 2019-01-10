1c61ac97903ffc806315f96ed5da55dd390ac8ab
phong <luctruongphong13@gmail.com>
2019-01-10 17:20:59 +0700

    Create TabNavigation

---
+++ app/src/screen/newfeed/NewFeedScreen.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+export default class NewFeedScreen extends Component {
+  render() {
+    const { navigate } = this.props.navigation;
+    return <Button title="Go to Time" />;
+  }
+}
--- app/src/stores/index.js
+++ app/src/stores/index.js
@@ -1,10 +1,7 @@
 import LoginStore from "./LoginStore";
-import OrientationListener from "./OrientationListener";
 
 const loginStore = new LoginStore();
-const orientationListener = new OrientationListener();
 
 export default {
-  loginStore: loginStore,
-  orientationListener: orientationListener
+  loginStore: loginStore
 };
